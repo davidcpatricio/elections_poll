e2cdaa08a0562953669ff6677795488c635eacb7
David Patrício <david.c.patricio@gmail.com>
2024-03-14 17:14:01 +0000

    Create model Party; configure media and static URL and root

--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Party(models.Model):
+    class Meta:
+        verbose_name_plural = 'Parties'
+
+    name = models.CharField(max_length=100)
+    initials = models.CharField(max_length=10)
+    icon = models.ImageField(
+        upload_to='product_images/%Y/%m/', blank=True, null=True)
+    votes = models.IntegerField(default=0)
