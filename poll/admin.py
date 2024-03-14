3aeea43c4f082d55fdbf932896509b91f4de5208
David Patrício <david.c.patricio@gmail.com>
2024-03-14 18:04:55 +0000

    Create model Profile

--- poll/admin.py
+++ poll/admin.py
@@ -4,3 +4,4 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Party)
+admin.site.register(models.Profile)
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 
@@ -13,3 +14,40 @@ class Party(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255)
+    id_number = models.CharField(max_length=9)
+    date_of_birth = models.DateField()
+    region = models.CharField(
+        max_length=2,
+        choices=(
+            ('AV', 'Aveiro'),
+            ('BE', 'Beja'),
+            ('BG', 'Braga'),
+            ('BN', 'Bragança'),
+            ('CB', 'Castelo Branco'),
+            ('CO', 'Coimbra'),
+            ('EV', 'Évora'),
+            ('FA', 'Faro'),
+            ('GU', 'Guarda'),
+            ('LE', 'Leiria'),
+            ('LX', 'Lisboa'),
+            ('PG', 'Portalegre'),
+            ('PO', 'Porto'),
+            ('SA', 'Santarém'),
+            ('SE', 'Setúbal'),
+            ('VC', 'Viana do Castelo'),
+            ('VR', 'Vila Real'),
+            ('VS', 'Viseu'),
+            ('AC', 'Açores'),
+            ('MA', 'Madeira'),
+            ('EU', 'Outros países da Europa'),
+            ('FE', 'Outros países fora da Europa'),
+        )
+    )
+
+    def __str__(self) -> str:
+        return self.full_name
