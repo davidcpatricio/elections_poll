ead1c922e9f993f08a386e88dfe4f2d9eec95414
David Patrício <david.c.patricio@gmail.com>
2024-03-25 12:01:39 +0000

    Configured URLs and views; created partial files for head and header

--- poll/urls.py
+++ poll/urls.py
@@ -6,4 +6,9 @@ app_name = 'poll'
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('user/register/', views.register, name="register"),
+    path('user/login/', views.login, name="login"),
+    path('user/logout/', views.logout, name="logout"),
+    path('user/update/', views.update, name="update"),
+    path('vote/', views.vote, name="vote"),
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,5 +1,35 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from poll.models import Party
 
 
 def index(request):
-    return render(request, 'poll/pages/index.html')
+    poll = Party.objects.order_by('-votes')
+
+    context = {
+        'poll': poll,
+        'page_title': ' | INDEX',
+    }
+
+    return render(request, 'poll/pages/index.html', context)
+
+
+def register(request):
+    return HttpResponse('REGISTER')
+
+
+def login(request):
+    return HttpResponse('LOGIN')
+
+
+def logout(request):
+    return HttpResponse('LOGOUT')
+
+
+def vote(request):
+    return HttpResponse('VOTE')
+
+
+def update(request):
+    return HttpResponse('UPDATE')
